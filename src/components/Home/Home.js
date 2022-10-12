e053df0bd389d45f5f70b3e2632812d60713abdd
Atik2788 <ashanto653.2@gmail.com>
2022-10-12 15:24:15 +0600

    final commit-1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Quiz from "../Quiz/Quiz";
+import './Home.css'
 
 const Home = () => {
   const quizes = useLoaderData();
@@ -9,7 +10,7 @@ const Home = () => {
     <div className="bg-red-50">
 
       <div>
-        <h2 className="text-5xl lg:py-20 py-5 font-bold">Test Youself with <span className="text-7xl text-orange-500">Quiz Hunt</span></h2>
+        <h2 className="text-5xl lg:py-20 py-5 font-bold animation-text">Test Youself with <span className="text-7xl text-orange-500">Quiz Hunt</span></h2>
       </div>
 
       <div className="lg:flex lg:gap-4 w-10/12 mx-auto pb-20">
